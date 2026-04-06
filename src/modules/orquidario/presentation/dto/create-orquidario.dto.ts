ff7bd2be349f6fb111f5d183433b137652967d28
amandesp <amanda.635@aluno.ueg.br>
2026-04-06 19:55:40 -0300

    Remoção do parâmetro id em create-orquidário.dto

--- src/modules/orquidario/presentation/dto/create-orquidario.dto.ts
+++ src/modules/orquidario/presentation/dto/create-orquidario.dto.ts
@@ -1,13 +1,10 @@
 import {ApiProperty} from '@nestjs/swagger'
 
 export class CreateOrquidarioDTO{
-    @ApiProperty()
-    id: number;
-
     @ApiProperty({example: 'Avenida Presidente Kennedy, Qd G Lte 10'})
     enderecoOrquidario: string;
 
-    @ApiProperty({example: '26-03-2026'})
+    @ApiProperty({example: '2026-03-20'})
     dataCriacao: Date;
 
     @ApiProperty({example: true})
