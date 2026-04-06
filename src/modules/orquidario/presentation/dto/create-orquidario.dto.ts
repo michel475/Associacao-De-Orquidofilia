5dc2480e2a4ff1bfa254343c38016a83bfcb1dd1
amandesp <amanda.635@aluno.ueg.br>
2026-04-06 19:55:40 -0300

    Remoção do parâmetro id em create-orquidário.dto

--- src/modules/orquidario/presentation/dto/create-orquidario.dto.ts
+++ src/modules/orquidario/presentation/dto/create-orquidario.dto.ts
@@ -5,9 +5,14 @@ export class CreateOrquidarioDTO{
     @ApiProperty({example: 'Avenida Presidente Kennedy, Qd G Lte 10'})
     enderecoOrquidario: string;
 
+<<<<<<< HEAD
     @ApiProperty({ example: '2026-03-26' })
     @IsDateString()
     dataCriacao: string;
+=======
+    @ApiProperty({example: '2026-03-20'})
+    dataCriacao: Date;
+>>>>>>> ff7bd2b (Remoção do parâmetro id em create-orquidário.dto)
 
     @ApiProperty({example: true})
     irrigadoAuto: boolean;
