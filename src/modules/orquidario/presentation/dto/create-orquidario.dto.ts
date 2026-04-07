3d54849ecae608cb2679ce62cc783913603892e1
michel475 <michaelchacal776@gmail.com>
2026-04-07 19:36:01 -0300

    sem mensagem

--- src/modules/orquidario/presentation/dto/create-orquidario.dto.ts
+++ src/modules/orquidario/presentation/dto/create-orquidario.dto.ts
@@ -5,17 +5,9 @@ export class CreateOrquidarioDTO{
     @ApiProperty({example: 'Avenida Presidente Kennedy, Qd G Lte 10'})
     enderecoOrquidario: string;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     @ApiProperty({ example: '2026-03-26' })
     @IsDateString()
     dataCriacao: string;
-=======
-=======
->>>>>>> 609c2b7b7a417dce1be783e72ec3b4ba01c78675
-    @ApiProperty({example: '2026-03-20'})
-    dataCriacao: Date;
->>>>>>> ff7bd2b (Remoção do parâmetro id em create-orquidário.dto)
 
     @ApiProperty({example: true})
     irrigadoAuto: boolean;
