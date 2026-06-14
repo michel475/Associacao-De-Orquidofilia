a079257fe4701d75d6f31ff087ff2883145caa95
amandesp <amanda.635@aluno.ueg.br>
2026-06-14 18:16:18 -0300

    inicio da implementação da lostagem de orquidario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { App } from './app';
 import { ReproducaoFlorList } from "./reproducao-flor-list/components/reproducao-flor-list"
 import { Home } from './home/components/home';
+import { OrquidarioListComponent } from './orqudario-list/components/orquidario-list';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'reproducaoFlor', component:ReproducaoFlorList},
+    {path: 'orquidario', component: OrquidarioListComponent}
 ];
---
+++ src/app/orqudario-list/model/orquidario.ts
@@ -0,0 +1,7 @@
+export class Orquidario{
+    id!: number;
+    endereco!: string;
+    dataCriacao!: Date;
+    irrigadoAuto!: boolean;
+    areaMQuadrados!: number;
+}
