7d718d4befb904c16e2076a72dc9f9bc67db6aca
Amanda Espíndola de Almeida <amanda.635@aluno.ueg.br>
2026-06-15 13:50:46 -0300

    início de criação do formulário de criação/edição de orquidário

--- src/app/orquidario-form/orquidario-form.ts
+++ src/app/orquidario-form/orquidario-form.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { Orquidario } from '../orquidario-list/model/orquidario';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-orquidario-form',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCard
+],
   templateUrl: './orquidario-form.html',
   styleUrl: './orquidario-form.css',
 })
-export class OrquidarioForm {}
+export class OrquidarioForm {
+  orquidario: Orquidario = new Orquidario();
+
+  constructor() {}
+
+  enviarFormulario() {
+    console.log(this.orquidario);
+  }
+}
