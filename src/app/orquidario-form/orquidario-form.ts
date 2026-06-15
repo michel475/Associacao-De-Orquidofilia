0a66f851abb9fe03ab64d4223b35cac8355a81d7
Amanda Espíndola de Almeida <amanda.635@aluno.ueg.br>
2026-06-15 15:17:08 -0300

    Teste de cores e disposição de campos no formulário

--- src/app/orquidario-form/orquidario-form.ts
+++ src/app/orquidario-form/orquidario-form.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCard } from '@angular/material/card';
+import { MatCard, MatCardHeader, MatCardTitle, MatCardContent } from '@angular/material/card';
 import { Orquidario } from '../orquidario-list/model/orquidario';
 import { FormsModule } from '@angular/forms';
 
@@ -15,7 +15,10 @@ import { FormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatCard
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent
 ],
   templateUrl: './orquidario-form.html',
   styleUrl: './orquidario-form.css',
