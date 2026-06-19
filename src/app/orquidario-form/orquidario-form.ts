dc38962453c180e075d229c9278db944df4612c9
Amanda Espíndola de Almeida <amanda.635@aluno.ueg.br>
2026-06-19 13:40:43 -0300

    fix: melhoria do formulário de orquidário

--- src/app/orquidario-form/orquidario-form.ts
+++ src/app/orquidario-form/orquidario-form.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCard, MatCardHeader, MatCardTitle, MatCardContent } from '@angular/material/card';
 import { Orquidario } from '../orquidario-list/model/orquidario';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 
 @Component({
   selector: 'app-orquidario-form',
@@ -18,8 +21,14 @@ import { FormsModule } from '@angular/forms';
     MatCard,
     MatCardHeader,
     MatCardTitle,
-    MatCardContent
+    MatCardContent,
+    MatDatepickerModule,
+    MatIconModule,
+    MatNativeDateModule,
 ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }  // ← ADICIONAR ISSO
+  ],
   templateUrl: './orquidario-form.html',
   styleUrl: './orquidario-form.css',
 })
