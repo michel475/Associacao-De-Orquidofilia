a5dcf238cb7154378ad18b91a2079da0f01e9d76
michel475 <michaelchacal776@gmail.com>
2026-06-20 17:38:20 -0300

    feat: modulo usuario e jwt auth strategies

---
+++ src/modules/auth/guards/roles.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable, CanActivate, ExecutionContext, Logger } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Role } from '../../users/entities/user.entity';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) { }
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    const logger = new Logger(RolesGuard.name);
+    logger.debug(`canActivate. Role: ${requiredRoles}`);
+    if (!requiredRoles) {
+      return true;
+    }
+    const { user } = context.switchToHttp().getRequest();
+    logger.debug(`user object from passport: ${JSON.stringify(user)}`);
+    logger.debug(`user.role: ${user?.role}`);
+    return requiredRoles.some((role) => user?.role === role);
+  }
+}
---
+++ src/modules/users/presentation/user.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Get, Post, Patch, Param, UseGuards, Logger, Request } from '@nestjs/common';
+import { UsersService } from '../application/user.service';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard'
+import { RolesGuard } from '../../auth/guards/roles.guard';
+import { Roles } from '../../auth/decorators/roles.decorator'
+import { Role } from '../user.entity';
+// import { RecoveryService } from '../auth/recovery.service';
+// import { MailerService } from '../mailer/mailer.service';
+
+@Controller('users')
+export class UsersController {
+
+  private readonly logger = new Logger(UsersController.name);
+
+  constructor(
+    private readonly usersService: UsersService,
+    // private readonly recoveryService: RecoveryService,
+    // private readonly mailerService: MailerService,
+  ) { }
+
+  @Get()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  findAll() {
+    this.logger.log('findAll');
+    return this.usersService.findAll();
+  }
+
+  @Patch(':id/activate')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  activate(@Param('id') id: string) {
+    return this.usersService.activate(id);
+  }
+
+//   @Post(':id/reset-password')
+//   @UseGuards(JwtAuthGuard, RolesGuard)
+//   @Roles(Role.ADMIN)
+//   async resetPassword(@Param('id') id: string) {
+//     const user = await this.usersService.findById(id);
+//     // const token = this.recoveryService.generateToken(user.email);
+//     // await this.mailerService.sendPasswordResetEmail(user.email, token);
+//     return { message: 'E-mail de recuperação de senha enviado com sucesso.' };
+}
